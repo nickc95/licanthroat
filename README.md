05185e713a79bdf5115e1e17e78a47a4bf8f7269
Nicholas C <nick.chim95@gmail.com>
2020-02-04 16:42:59 -0800

    Add test events

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # licanthroat
+
+add bot to server using https://discordapp.com/oauth2/authorize?&client_id=673727161752092673&scope=bot&permissions=2064
--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"fmt"
 	"github.com/bwmarrin/discordgo"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
 )
 
 func init() {
@@ -10,9 +14,69 @@ func init() {
 }
 
 func main() {
-	_, err := discordgo.New("Bot " + envConfig.BotToken)
+	discord, err := discordgo.New("Bot " + envConfig.BotToken)
 	if err != nil {
-		fmt.Println(err.Error())
+		fmt.Println(err)
 		return
 	}
+
+	discord.AddHandler(ready)
+	discord.AddHandler(messageCreate)
+	discord.AddHandler(guildCreate)
+
+	err = discord.Open()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println("now running")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	discord.Close()
+}
+
+// sent when client completes initial handshake with gateway for a new session.
+func ready(s *discordgo.Session, event *discordgo.Ready) {
+	fmt.Println("Ready")
+}
+
+// sent when a message is created, m is a message struct
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	// ignore messages created by itself
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if strings.HasPrefix(m.Content, "!help") {
+		channel, err := s.State.Channel(m.ChannelID)
+		if err != nil {
+			fmt.Println("couldn't find corresponding channel")
+			return
+		}
+
+		// guild, err = s.State.Guild(channel.GuildID)
+		// if err != nil {
+		// 	fmt.Println("couldn't find corresponding guild")
+		// 	return
+		// }
+
+		_, _ = s.ChannelMessageSend(channel.ID, "i'm sending a message corresponding to the message !help")
+		return
+	}
+}
+
+// sent when a new guild is joined
+func guildCreate(s *discordgo.Session, event *discordgo.GuildCreate) {
+	if event.Guild.Unavailable {
+		return
+	}
+
+	// for _, channel := range event.Guild.Channels {
+	// 	if channel.ID == event.Guild.ID {
+	// 		_, _ = s.ChannelMessageSend(channel.ID, "licanthroat is ready! Type nothing because nothing is supported yet.")
+	// 		return
+	// 	}
+	// }
 }
